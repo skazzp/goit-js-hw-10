2fe927edda9a92e453a5bada6e1dfa4610042b54
skazzp <skazzp@gmail.com>
2022-08-20 15:23:10 +0300

    task check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
-
+var debounce = require('lodash.debounce');
+import { fetchCountries } from './modules/fetchCountries.js';
+import { refs } from './modules/refs.js';
 const DEBOUNCE_DELAY = 300;
+
+refs.searchBox.addEventListener(
+  'input',
+  debounce(fetchCountries, DEBOUNCE_DELAY)
+);
