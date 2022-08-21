43dc5210b43aae2492b13dd7821bebb962229e81
skazzp <skazzp@gmail.com>
2022-08-21 22:53:15 +0300

    axios

--- src/modules/refs.js
+++ src/modules/refs.js
@@ -1,5 +1,4 @@
 export const refs = {
-  searchBox: document.querySelector('#search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+  searchForm: document.querySelector('#search-form'),
+  searchQuery: document.querySelector("[name='searchQuery']"),
 };
